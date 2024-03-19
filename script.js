a91e7795ee2fb930e091f4b4e9c40228bec6b156
nstcuz <126234425+nstcuz@users.noreply.github.com>
2024-03-18 21:28:27 -0700

    added music, finished basic game functionality

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // HTML elements
 // const gameBoard = document.getElementById('game-board');
-const fullCard = document.querySelectorAll('.card');
+// const fullCard = document.querySelectorAll('.card');
 const startButton = document.getElementById('start-button');
 const cardFront = document.querySelectorAll('.card-front');
 const cardBack = document.querySelectorAll('.card-back');
+const restartGameButton = document.getElementById('restartGame');
+const endGamePopUp= document.getElementById('popUp');
+// let music = document.getElementById("backgroundAudio");
 
+//array to hold the value that we will be using to check if matched
+let checkIfMatched = [];
+// array to count how many successful matches
+let successfulMatch = [];
+// will hold the value of the cardFront for the selected cards
+let card1;
+let card2;
+//lets us know if its first card or second card that is being clicked
+let cardRevealClickIndex = 0;
+
+
+//create card object
 class Card {
     constructor(path, alt, number) {
         this.path = path;
@@ -46,53 +61,44 @@ function shuffleCards(cards){
 };
 // console.log(shuffledCards);
 
-// idk how im gonna lay this out tbh
-//but for now heres the start butotn
-startButton.addEventListener('click', () => {
-    console.log('game started!');
-    shuffleCards(cards);
-    // console.log(cards);
+// call the start button
+start();
 
-    // card reveal
+//start butotn
+function start(){
+    startButton.addEventListener('click', () => {
+    console.log('game started!');
+    musicTime();
+    shuffleCards(cards);
+    // card reveal => check if ready for match => check if match
     startCardReveal();
 });
-
-//array to hold the value that we will be using to check if matched
-let isMatched = [];
-//stores index so we can access the card number in other functions THIS IS CHANGED NOW 
-let card1;
-let card2;
-
-//lets us know if its first card or second card that is being clicked
-let cardRevealClickIndex = 0;
+};
 
 // reveal card when you click on the specific card
-//event listener applied forEach() to all cards when game is started
 function startCardReveal(){
     cardBack.forEach(function(cardBack, i){
             // target the front side of the card that was clicked and display
             const cardFront = cardBack.previousElementSibling;
-            // set card stylings (hide back, display front). Then apply src & alt to the front
+            // apply src & alt to the card front
             cardFront.src = cards[i].path;
             cardFront.alt = cards[i].alt;
 
         cardBack.addEventListener('click', (e) => {
-            // target the card that was clicked and hide
+            // target the card that was clicked and hide it
             e.currentTarget.style.display = 'none';
-            
-            // set card stylings (hide back, display front). Then apply src & alt to the front
+            // set card stylings (display front).
             cardFront.style.display = 'block';
-            
-            // this writes the index number to either cardIndex1 or cardIndex2 so that we can target the specific card in another function
+            // keeps track if its card1 or card2 for matching
             if(cardRevealClickIndex === 0){
                 card1 = cardFront;
                 cardRevealClickIndex++;
-                isMatched.push(card1);
+                checkIfMatched.push(card1);
             } else {
                 card2 = cardFront;
-                // cardRevealClickIndex = 0;
-                isMatched.push(card2);
+                checkIfMatched.push(card2);
             }
+            //confirms 2 cards have been clicked
             checkIfReadyForMatch(card1, card2);
         });
     });
@@ -101,25 +107,82 @@ function startCardReveal(){
 // lets check and see if there is 2 items in the array to match
 // if there is we will call the function to see if cards are a pair
 function checkIfReadyForMatch(card1, card2){
-    if(isMatched.length === 2){
+    if(checkIfMatched.length === 2){
         confirmMatch(card1, card2);
     };
 };
 
 // check if the first card matches the second card
-// if not reset styles && erase array contents
 function confirmMatch(card1, card2){
     if(card1.src !== card2.src){
+        setTimeout(function(){
         // set stylings for card1
-        card1.style.display = 'none';
-        card1.nextElementSibling.style.display = 'block';
-        // set stylings for card2
-        card2.style.display = 'none';
-        card2.nextElementSibling.style.display = 'block';
+            card1.style.display = 'none';
+            card1.nextElementSibling.style.display = 'block';
+            // set stylings for card2
+            card2.style.display = 'none';
+            card2.nextElementSibling.style.display = 'block';
+        }, 750);
     } else {
-        isMatched.length = 0;
-        // console.log(isMatched);
+        successfulMatch.push('match');
     };
+    // reset index to check if 2 cards have been clicked
     cardRevealClickIndex = 0; 
-    isMatched = [];
+    // reset the array holding the cards
+    checkIfMatched = [];
+    // if the array has all matched cards call the next function
+    if(successfulMatch.length === 6){
+        allMatched()
+    };
+};
+
+function allMatched(){
+    console.log('all matched!');
+        // stop music
+    let music = document.getElementById("backgroundAudio");
+    music.volume = 0.05;
+    endGame();
+};
+
+function endGame(){
+    showPopup();
+    // lower music volume
+    let music = document.getElementById("backgroundAudio");
+    music.volume = 0.05;
+};
+
+function showPopup() {
+    const endGamePopUp = document.getElementById('popUp');
+    endGamePopUp.style.display = 'block';
+};
+
+function gameReset(){
+    //empty arrays and index
+    successfulMatch = [];
+    checkIfMatched = [];
+    cardRevealClickIndex = 0;
+
+    //reset html
+    cardBack.forEach( el => {
+        el.style.display = 'block';
+    });
+    cardFront.forEach(el => {
+        el.style.display = 'none';
+    });
+
+    endGamePopUp.style.display = 'none';
+};
+
+restartGameButton.addEventListener('click', () => {
+    gameReset();
+    start();
+});
+
+//init music
+function musicTime(){
+    if(successfulMatch.length === 6){
+        let music = document.getElementById("backgroundAudio");
+        music.play();
+        music.volume = 0.25;
+    };
 };
